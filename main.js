1a8a4882f07db2c2ffef61d259f98fa6d4c31a6c
root <root@localhost.localdomain>
2019-08-27 14:53:04 -0300

    extends

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-class TodoList {
+class List {
     constructor() {
-        this.todos = [];
+        this.data = [];
     }
 
-    addTodo() {
-        this.todos.push('Novo Todo');
-        alert(this.todos);
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class Matematica {
+    static soma(a, b) {
+        return a + b;
+    }
+
+    static multiplicacao(a, b) {
+        return a * b;
+    }
+}
+
+
+class TodoList extends List {
+
+}
+
+
 
 const MinhaLista = new TodoList();
 
 document.getElementById('add-todo').onclick = function() {
-    MinhaLista.addTodo();
+    MinhaLista.add("2019-08-27");
 };
+
+console.log("Soma: " + Matematica.soma(10, 17));
