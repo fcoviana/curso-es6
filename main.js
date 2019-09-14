d46089576e5bc7cb4336a74fc9c08c07fac43857
francisco1030 <franciscoviana862@gmail.com>
2019-09-14 03:38:16 -0300

    webspack server

--- main.js
+++
@@ -1,6 +0,0 @@
-import * as funcoes from './funcoes';
-//import  soma, { sub } from './funcoes';
-//import somaFunc from './soma';
-
-console.log(funcoes.soma(10,30));
-console.log(funcoes.sub(100,30));
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import * as funcoes from './model/funcoes';
+//import  soma, { sub } from './funcoes';
+//import somaFunc from './soma';
+
+console.log(funcoes.soma(10,30));
+console.log(funcoes.sub(100,30));
+
+alert(funcoes.sub(100,30));
